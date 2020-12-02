0d748bc658119f28c2aa5a9c422de648b0de07f8
Ivan Jorgovanovic <jorgovanovicivan@gmail.com>
2020-12-02 15:35:14 +0100

    Kreirana osnovna aplikacija

---
+++ database/migrations/2020_12_02_124652_drop_todotest_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropTodotestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('todotests');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todotests');
+
+    }
+}
